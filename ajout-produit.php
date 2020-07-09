f874b8ef8580e52f3cb54f425a22dbaa08fb3fba
Tom Schneider <tom.schneider@iseg.fr>
2020-07-09 15:29:25 +0200

    pushy

---
+++ ajout-produit.php
@@ -0,0 +1,24 @@
+<?php
+
+function connect_to_database()
+{
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$databasename = "BaseTest01";
+
+try
+{
+    $pdo = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
+    $pdo->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    insert_into("id8");
+    value("T-shirt noir", "t-shirt coton de couleur noir", "15.50","10");
+    return $pdo;
+
+}
+
+}
+
+
+
+?>
